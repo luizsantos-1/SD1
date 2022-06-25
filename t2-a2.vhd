8b9a13a73221c60df460b3b45e0ac6df2ddcf68b
luizsantos-1 <64617480+luizsantos-1@users.noreply.github.com>
2022-06-24 22:45:18 -0300

    Update t2-a2.vhd

--- t2-a2.vhd
+++ t2-a2.vhd
@@ -1,4 +1,4 @@
-elibrary IEEE;
+library IEEE;
 use IEEE.numeric_bit.all;
 
 entity purrinha is 
@@ -66,11 +66,14 @@ end flipflopd;
 
 architecture behavior of flipflopd is 
 begin
-  process (reset, clock) begin
-    if reset=’0’ then Q <= ’0’;
-    elsif clock’EVENT and clock=’1’ and EN=’1’ then Q <= D;
-    end if;
-  end process ;
+  process (reset, clock) 
+  	begin
+      if reset='0' 
+      	then Q <= '0';
+      elsif clock'EVENT and clock='1' and EN='1' 
+      	then Q <= D;
+      end if;
+  end process;
 end behavior;
 
 entity purrinha_plus is port(
@@ -91,6 +94,7 @@ architecture purrinha_plus_arch of flipflopd is
 	
     signal num1_guardado, num2_guardado, num3_guardado, num4_guardado: bit_vector(1 downto 0);
     signal guess1_guardado, guess2_guardado, guess3_guardado, guess4_guardado: bit_vector(3 downto 0);
+    signal display_i: bit_vector(6 downto0);
     
     component purrinha is port (
     num4, num3, num2, num1: in bit_vector(1 downto 0); 
@@ -106,40 +110,48 @@ architecture purrinha_plus_arch of flipflopd is
     
 	begin
     
-    ff_num10: flipflopd port map (num1(0), reset, clock, , num1_guardado(0));
-    ff_num11: flipflopd port map (num1(1), reset, clock, , num1_guardado(1));
+    ff_num10: flipflopd port map (num1(0), reset, clock, load1, num1_guardado(0));
+    ff_num11: flipflopd port map (num1(1), reset, clock, load1, num1_guardado(1));
     
-    ff_num20: flipflopd port map (num2(0), reset, clock, , num2_guardado(0));
-    ff_num21: flipflopd port map (num2(1), reset, clock, , num2_guardado(1));
+    ff_num20: flipflopd port map (num2(0), reset, clock, load2, num2_guardado(0));
+    ff_num21: flipflopd port map (num2(1), reset, clock, load2, num2_guardado(1));
     
-    ff_num30: flipflopd port map (num3(0), reset, clock, , num3_guardado(0));
-    ff_num31: flipflopd port map (num3(1), reset, clock, , num3_guardado(1));
+    ff_num30: flipflopd port map (num3(0), reset, clock, load3, num3_guardado(0));
+    ff_num31: flipflopd port map (num3(1), reset, clock, load3, num3_guardado(1));
     
-    ff_num40: flipflopd port map (num4(0), reset, clock, , num4_guardado(0));
-    ff_num41: flipflopd port map (num4(1), reset, clock, , num4_guardado(1));
+    ff_num40: flipflopd port map (num4(0), reset, clock, load4, num4_guardado(0));
+    ff_num41: flipflopd port map (num4(1), reset, clock, load4, num4_guardado(1));
     
-    ff_guess10: flipflopd port map (guess1(0), reset, clock, , guess1_guardado(0));
-    ff_guess11: flipflopd port map (guess1(1), reset, clock, , guess1_guardado(1));
-    ff_guess12: flipflopd port map (guess1(2), reset, clock, , guess1_guardado(2));
-    ff_guess13: flipflopd port map (guess1(3), reset, clock, , guess1_guardado(3));
+    ff_guess10: flipflopd port map (guess1(0), reset, clock, load1, guess1_guardado(0));
+    ff_guess11: flipflopd port map (guess1(1), reset, clock, load1, guess1_guardado(1));
+    ff_guess12: flipflopd port map (guess1(2), reset, clock, load1, guess1_guardado(2));
+    ff_guess13: flipflopd port map (guess1(3), reset, clock, load1, guess1_guardado(3));
     
-    ff_guess20: flipflopd port map (guess2(0), reset, clock, , guess2_guardado(0));
-    ff_guess21: flipflopd port map (guess2(1), reset, clock, , guess2_guardado(1));
-    ff_guess22: flipflopd port map (guess2(2), reset, clock, , guess2_guardado(2));
-    ff_guess23: flipflopd port map (guess2(3), reset, clock, , guess2_guardado(3));
+    ff_guess20: flipflopd port map (guess2(0), reset, clock, load2, guess2_guardado(0));
+    ff_guess21: flipflopd port map (guess2(1), reset, clock, load2, guess2_guardado(1));
+    ff_guess22: flipflopd port map (guess2(2), reset, clock, load2, guess2_guardado(2));
+    ff_guess23: flipflopd port map (guess2(3), reset, clock, load2, guess2_guardado(3));
     
-    ff_guess30: flipflopd port map (guess3(0), reset, clock, , guess3_guardado(0));
-    ff_guess31: flipflopd port map (guess3(1), reset, clock, , guess3_guardado(1));
-    ff_guess32: flipflopd port map (guess3(2), reset, clock, , guess3_guardado(2));
-    ff_guess33: flipflopd port map (guess3(3), reset, clock, , guess3_guardado(3));
+    ff_guess30: flipflopd port map (guess3(0), reset, clock, load3, guess3_guardado(0));
+    ff_guess31: flipflopd port map (guess3(1), reset, clock, load3, guess3_guardado(1));
+    ff_guess32: flipflopd port map (guess3(2), reset, clock, load3, guess3_guardado(2));
+    ff_guess33: flipflopd port map (guess3(3), reset, clock, load3, guess3_guardado(3));
     
-    ff_guess40: flipflopd port map (guess4(0), reset, clock, , guess4_guardado(0));
-    ff_guess41: flipflopd port map (guess4(1), reset, clock, , guess4_guardado(1));
-    ff_guess42: flipflopd port map (guess4(2), reset, clock, , guess4_guardado(2));
-    ff_guess43: flipflopd port map (guess4(3), reset, clock, , guess4_guardado(3));
+    ff_guess40: flipflopd port map (guess4(0), reset, clock, load4, guess4_guardado(0));
+    ff_guess41: flipflopd port map (guess4(1), reset, clock, load4, guess4_guardado(1));
+    ff_guess42: flipflopd port map (guess4(2), reset, clock, load4, guess4_guardado(2));
+    ff_guess43: flipflopd port map (guess4(3), reset, clock, load4, guess4_guardado(3));
     
     p1: purrinha port map (num1_guardado, num2_guardado, num3_guardado, num4_guardado, 
     						guess1_guardado, guess2_guardado, guess3_guardado, guess4_guardado, 
-                            display); -- CORRIGIR
+                            display_i);
+                            
+    ff_display0: flipflopd port map (display_i(0), reset, clock, atualiza, display(0));
+    ff_display1: flipflopd port map (display_i(1), reset, clock, atualiza, display(1));
+    ff_display2: flipflopd port map (display_i(2), reset, clock, atualiza, display(2));
+    ff_display3: flipflopd port map (display_i(3), reset, clock, atualiza, display(3));
+    ff_display4: flipflopd port map (display_i(4), reset, clock, atualiza, display(4));
+    ff_display5: flipflopd port map (display_i(5), reset, clock, atualiza, display(5));
+    ff_display6: flipflopd port map (display_i(6), reset, clock, atualiza, display(6));
   	
 end purrinha_plus_arch;
